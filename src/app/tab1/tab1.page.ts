c6afda336936928febe3c79b46b001ca56931753
Cauê Almeida <nncl@live.com>
2021-07-15 13:34:54 -0300

    feat: Setting up Konva example
    - Installing Konva through npm

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Konva from 'konva';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: [ 'tab1.page.scss' ]
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.build();
+  }
+
+  build(): void {
+    const stage = new Konva.Stage({
+      container: 'container',
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    // add canvas element
+    const layer = new Konva.Layer();
+    stage.add(layer);
+
+    // create shape
+    const box = new Konva.Rect({
+      x: 50,
+      y: 50,
+      width: 100,
+      height: 50,
+      fill: '#00D2FF',
+      stroke: 'black',
+      strokeWidth: 4,
+      draggable: true,
+    });
+
+    layer.add(box);
+
+    // add cursor styling
+    box.on('mouseover', () => {
+      document.body.style.cursor = 'pointer';
+    });
+    box.on('mouseout', () => {
+      document.body.style.cursor = 'default';
+    });
+  }
 
 }
