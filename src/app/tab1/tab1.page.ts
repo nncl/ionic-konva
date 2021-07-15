884a40bbe0dc64da264a20fa35ef1c668f774c36
Cauê Almeida <nncl@live.com>
2021-07-15 18:06:01 -0300

    feat: Centering canvas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import Konva from 'konva';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: [ 'tab1.page.scss' ]
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page implements AfterViewInit {
+  @ViewChild('content') content: any;
 
   constructor() {
   }
 
-  ngOnInit(): void {
-    this.build();
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const height = this.content.el.clientHeight;
+      this.build(height);
+    }, 1000);
   }
 
-  build(): void {
+  build(height = 0): void {
 
     Konva.hitOnDragEnabled = true;
 
@@ -26,7 +31,7 @@ export class Tab1Page implements OnInit {
      */
 
     const width = window.innerWidth;
-    const height = window.innerHeight;
+    // const height = window.innerHeight;
 
     const stage = new Konva.Stage({
       container: 'container',
@@ -45,8 +50,8 @@ export class Tab1Page implements OnInit {
      */
     Konva.Image.fromURL('https://dummyimage.com/300x800', (userImage) => {
       userImage.setAttrs({
-        x: 0,
-        y: 0,
+        x: (width / 2) - 140,
+        y: (height / 2) - 140,
         width: 280,
         height: 280,
         draggable: true
@@ -57,8 +62,8 @@ export class Tab1Page implements OnInit {
     // Moldura
     Konva.Image.fromURL('/assets/moldura.png', (mockup) => {
       mockup.setAttrs({
-        x: 0,
-        y: 0,
+        x: (width / 2) - 140,
+        y: (height / 2) - 140,
         width: 280,
         height: 280,
         id: 'canvas-moldura',
@@ -70,7 +75,7 @@ export class Tab1Page implements OnInit {
     // Texto
     var textNode: any = new Konva.Text({
       x: stage.width() / 2,
-      y: 15,
+      y: (height / 2) - 15,
       text: 'Simple Text',
       fontSize: 30,
       fontFamily: 'Calibri',
@@ -108,155 +113,12 @@ export class Tab1Page implements OnInit {
         fontSize: 60,
         fill: 'red',
       });
-      return;
-      // hide text node and transformer:
-      textNode.hide();
-      tr.hide();
-
-      // create textarea over canvas with absolute position
-      // first we need to find position for textarea
-      // how to find it?
-
-      // at first lets find position of text node relative to the stage:
-      var textPosition = textNode.absolutePosition();
-
-      // so position of textarea will be the sum of positions above:
-      var areaPosition = {
-        x: stage.container().offsetLeft + textPosition.x,
-        y: stage.container().offsetTop + textPosition.y,
-      };
-
-      // create textarea and style it
-      var textarea = document.createElement('textarea');
-      document.body.appendChild(textarea);
-
-      // apply many styles to match text on canvas as close as possible
-      // remember that text rendering on canvas and on the textarea can be different
-      // and sometimes it is hard to make it 100% the same. But we will try...
-      textarea.value = textNode.text();
-      textarea.style.position = 'absolute';
-      textarea.style.top = areaPosition.y + 'px';
-      textarea.style.left = areaPosition.x + 'px';
-      textarea.style.width = textNode.width() - textNode.padding() * 2 + 'px';
-      textarea.style.height =
-        textNode.height() - textNode.padding() * 2 + 5 + 'px';
-      textarea.style.fontSize = textNode.fontSize() + 'px';
-      textarea.style.border = 'none';
-      textarea.style.padding = '0px';
-      textarea.style.margin = '0px';
-      textarea.style.overflow = 'hidden';
-      textarea.style.background = 'none';
-      textarea.style.outline = 'none';
-      textarea.style.resize = 'none';
-      textarea.style.lineHeight = textNode.lineHeight();
-      textarea.style.fontFamily = textNode.fontFamily();
-      textarea.style.transformOrigin = 'left top';
-      textarea.style.textAlign = textNode.align();
-      textarea.style.color = textNode.fill();
-      let rotation = textNode.rotation();
-      var transform = '';
-      if (rotation) {
-        transform += 'rotateZ(' + rotation + 'deg)';
-      }
-
-      var px = 0;
-      // also we need to slightly move textarea on firefox
-      // because it jumps a bit
-      var isFirefox =
-        navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-      if (isFirefox) {
-        px += 2 + Math.round(textNode.fontSize() / 20);
-      }
-      transform += 'translateY(-' + px + 'px)';
-
-      textarea.style.transform = transform;
-
-      // reset height
-      textarea.style.height = 'auto';
-      // after browsers resized it we can set actual value
-      textarea.style.height = textarea.scrollHeight + 3 + 'px';
-
-      textarea.focus();
-
-      function removeTextarea() {
-        textarea.parentNode.removeChild(textarea);
-        window.removeEventListener('click', handleOutsideClick);
-        textNode.show();
-        tr.show();
-        tr.forceUpdate();
-      }
-
-      function setTextareaWidth(newWidth) {
-        if (!newWidth) {
-          // set width for placeholder
-          newWidth = textNode.placeholder.length * textNode.fontSize();
-        }
-        // some extra fixes on different browsers
-        var isSafari = /^((?!chrome|android).)*safari/i.test(
-          navigator.userAgent
-        );
-        if (isSafari) {
-          newWidth = Math.ceil(newWidth);
-        }
-
-        // var isEdge =
-        //   document.documentMode || /Edge/.test(navigator.userAgent);
-        // if (isEdge) {
-        //   newWidth += 1;
-        // }
-        textarea.style.width = newWidth + 'px';
-      }
-
-      textarea.addEventListener('keydown', function (e) {
-        // hide on enter
-        // but don't hide on shift + enter
-        if (e.keyCode === 13 && !e.shiftKey) {
-          textNode.text(textarea.value);
-          removeTextarea();
-        }
-        // on esc do not set value back to node
-        if (e.keyCode === 27) {
-          removeTextarea();
-        }
-      });
-
-      textarea.addEventListener('keydown', function (e) {
-        let scale = textNode.getAbsoluteScale().x;
-        setTextareaWidth(textNode.width() * scale);
-        textarea.style.height = 'auto';
-        textarea.style.height =
-          textarea.scrollHeight + textNode.fontSize() + 'px';
-      });
-
-      function handleOutsideClick(e) {
-        if (e.target !== textarea) {
-          textNode.text(textarea.value);
-          removeTextarea();
-        }
-      }
-
-      setTimeout(() => {
-        window.addEventListener('click', handleOutsideClick);
-      });
-    });
-
-    textNode.on('tap', () => {
-      console.log('tapppp'); // FIXME
     });
 
     setTimeout(() => {
       layer.add(textNode);
       layer.add(tr);
     }, 3000);
-
-    // TODO
-    // setTimeout(() => {
-    //   tr.remove();
-    // }, 10000);
-    //
-    // setTimeout(() => {
-    //   layer.add(tr);
-    // }, 12000);
 
     // PINCH
     let lastDist = 0;
